471bb51b24a8eb7ad39c1c52f22fe4ce9f088338
Gabriel de Paula Santos <gabriel.santos@outlook.com>
2022-04-18 00:27:29 -0400

    Creating the project and adding dependancies as well as connecting to the github api

---
+++ 01-Github-Explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(props) {
+    return (
+        <li>
+            <strong>{props.repository.full_name ?? 'Default'}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.html_url}>
+                Access Repository
+            </a>
+        </li>
+    );
+}
